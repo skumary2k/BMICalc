82699796954430e68b5a254893c818a37c596911
skumary2k <skumary2k@gmail.com>
2014-10-15 18:58:15 +0530

    Initial creation.

---
+++ FitnessMineApp/Helper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FitnessMineApp
+{
+    public static class Helper
+    {
+        public static double CalculateBMI(int ft, int inc, int we, WeightType wt)
+        {
+            double result = 0;
+
+            if (wt == WeightType.Pounds)
+            {
+                //Formula for BMI = ( Weight in Pounds / ( Height in inches x Height in inches ) ) x 703
+                var height = CnvertToInch(ft, inc);
+                result = (we / (height * height)) * 703;
+            }
+
+            if (wt == WeightType.Kilograms)
+            {
+                //Formula for BMI = ( Weight in Kilograms / ( Height in Meters x Height in Meters ) )
+                var height = ConvertToMeter(ft, inc);
+                result = (we / (height * height));
+            }
+
+            return result;
+        }
+
+        private static double CnvertToInch(int ft, int inc)
+        {
+            return (ft * 12) + inc; //1 ft = 12 inch
+        }
+
+        private static double ConvertToMeter(int ft, int inc)
+        {
+            return (ft * 0.3048) + (inc * 0.0254); //1 ft = 0.3048 meter & 1 inch = 0.0254 meter
+        }
+    }
+
+    public enum WeightType
+    {
+        Pounds = 0,
+        Kilograms = 1
+    }
+}
---
+++ FitnessMineApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace FitnessMineApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbWeight.Text))
+                return;
+            if (string.IsNullOrEmpty(tbHeightFeet.Text) && string.IsNullOrEmpty(tbHeightInch.Text))
+                return;
+
+            int ft = Convert.ToInt32(tbHeightFeet.Text);
+            int inc = Convert.ToInt32(tbHeightInch.Text);
+            int we = Convert.ToInt32(tbWeight.Text);
+            WeightType wt = cbWeightType.SelectedIndex == 0 ? WeightType.Pounds : WeightType.Kilograms;
+
+            var result = Helper.CalculateBMI(ft, inc, we, wt);
+
+            greetingOutput.Text = "You have a BMI of " +  result;
+        }
+    }
+}
